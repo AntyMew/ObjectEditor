9ae9474e8a69e0de9a25bc7e1d3ce1bd395eac7f
Callie LeFave <antymew@gmail.com>
2019-05-26 20:33:58 -0400

    Initialize project

---
+++ ObjectEditor/ObjectEditor.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="ObjectEditor.cs" company="Callie LeFave">
+// Copyright (c) Callie LeFave 2019
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using System;
+using ReactiveUI;
+
+namespace ObjectEditor
+{
+    /// <summary>
+    ///  A <see cref="ReactiveObject"/> representing a single <see cref="Type"/> and its serializable members.
+    /// </summary>
+    public class ObjectEditor : ReactiveObject
+    {
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ObjectEditor
+![GitHub](https://img.shields.io/github/license/AntyMew/ObjectEditor.svg)
+
 Cross-platform .NET library for making object editor GUIs.
